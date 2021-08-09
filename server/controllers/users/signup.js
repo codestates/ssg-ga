f8f22df6a14667661385ce608397c87ad22e9063
Hsource39 <source390@gmail.com>
2021-08-09 22:39:54 +0900

    add:signin 기능구현, modify: signup bcrypt 기능 추가

--- server/controllers/users/signup.js
+++ server/controllers/users/signup.js
@@ -1,7 +1,18 @@
 const { user } = require("../../db/models");
+const bcyrpt = require("bcrypt");
+const saltRounds = 5;
 
 module.exports = (req, res) => {
   const { email, username, password } = req.body;
+  bcyrpt.genSalt(saltRounds, function (err, salt) {
+    if (err) return next(err);
+    bcyrpt.hash(password, salt, function (err, hash) {
+      if (err) return next(err);
+      password = hash;
+      next();
+    });
+  });
+
   if (!email || !username || !password) {
     res.status(422).send("insufficient parameters supplied");
   }
