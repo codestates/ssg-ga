f99e0e03b00d9de5e44ff3ba305fa1595256f1a4
kimeensung <kimeensung@gmail.com>
2021-08-09 16:17:09 +0900

    server/add: article 게시,수정,삭제,가져오기 기능 추가

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,3 +1,8 @@
 module.exports = {
   signup: require("./users/signup"),
+
+  postArticle: require("./articles/article").post,
+  getSingleArticle: require("./articles/article").get,
+  editArticle: require("./articles/article").patch,
+  deleteArticle: require("./articles/article").delete,
 };
--- server/index.js
+++ server/index.js
@@ -19,6 +19,12 @@ app.get("/", (req, res) => {
 
 app.post("/user/signup", controllers.signup);
 
+app.post("/article", controllers.postArticle);
+app.get("/article/id/:articleId", controllers.getSingleArticle);
+app.patch("/article/id/:articleId", controllers.editArticle);
+app.delete("/article/id/:articleId", controllers.deleteArticle);
+
+
 app.listen(4000, () => {
   console.log(`Example app listening at http://localhost:4000`);
 });
