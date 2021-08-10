c9b530d66ebd9483012c693669b86014d691f993
kimeensung <kimeensung@gmail.com>
2021-08-10 17:01:45 +0900

    server/add: like 버튼 관련 기능 구현 및 세부 수정

--- server/controllers/articles/articlelist.js
+++ server/controllers/articles/articlelist.js
@@ -2,132 +2,137 @@ const { user, article } = require("../../db/models");
 const { Op } = require("sequelize");
 
 module.exports = async (req, res) => {
-  if (!req.query.type) {
-    // 상위 태그 및 재료 검색
-    const tagsAndIngredients = await article.findAll({
-      attributes: ['tag', 'ingredient'],
-      limit: 20,  // 최근 20개 게시물 기준으로 검색
-      order: [
-        ['createdAt', 'DESC'],
-      ],
-    });
-    const tags = {};
-    const ingredients = {};
-    // 게시물에서 태그와 재료 분류해 가져오기
-    tagsAndIngredients.forEach(data => {
-      data.tag = JSON.parse(data.tag);
-      data.tag.forEach(tag => {
-        tag in tags ? tags[tag] = tags[tag] + 1 : tags[tag] = 0;
+  try {
+    if (!req.query.type) {
+      // 상위 태그 및 재료 검색
+      const tagsAndIngredients = await article.findAll({
+        attributes: ['tag', 'ingredient'],
+        limit: 20,  // 최근 20개 게시물 기준으로 검색
+        order: [
+          ['createdAt', 'DESC'],
+        ],
       });
-      data.ingredient = JSON.parse(data.ingredient);
-      data.ingredient.forEach(ingredient => {
-        ingredient in ingredients ? ingredients[ingredient] = ingredients[ingredient] + 1 : ingredients[ingredient] = 0;
+      const tags = {};
+      const ingredients = {};
+      // 게시물에서 태그와 재료 분류해 가져오기
+      tagsAndIngredients.forEach(data => {
+        data.tag = JSON.parse(data.tag);
+        data.tag.forEach(tag => {
+          tag in tags ? tags[tag] = tags[tag] + 1 : tags[tag] = 0;
+        });
+        data.ingredient = JSON.parse(data.ingredient);
+        data.ingredient.forEach(ingredient => {
+          ingredient in ingredients ? ingredients[ingredient] = ingredients[ingredient] + 1 : ingredients[ingredient] = 0;
+        })
       })
-    })
-    // 상위 태그 배열로 만들어 sort
-    const tagsArr = [];
-    for (let el in tags) {
-      tagsArr.push([el, tags[el]]);
-    }
-    for (let i = 0; i < tagsArr.length; i++) {
-      tagsArr.sort((a, b) => b[1] - a[1]);
-    }
-    // 상위 재료 배열로 만들어 sort
-    const ingredientsArr = [];
-    for (let el in ingredients) {
-      ingredientsArr.push([el, ingredients[el]]);
-    }
-    for (let i = 0; i < ingredientsArr.length; i++) {
-      ingredientsArr.sort((a, b) => b[1] - a[1]);
-    }
-    // 게시물 최신순으로 전달
-    const articleList = await article.findAll({
-      attributes: ['id', 'title', 'thumbnail_type', 'thumbnail_color', 'ingredient'],
-      limit: 10,
-      offset: 0,
-      order: [
-        ['createdAt', 'DESC'],
-      ],
-    })
-    articleList.forEach(data => {
-      data.thumbnail_color = JSON.parse(data.thumbnail_color);
-      data.ingredient = JSON.parse(data.ingredient);
-    })
-    res.status(200).json({
-      data: {
-        tags: tagsArr.slice(0, 4),  // 상위 태그 5개
-        ingredients: ingredientsArr.slice(0, 4),  // 상위 재료 5개
-        articleList
+      // 상위 태그 배열로 만들어 sort
+      const tagsArr = [];
+      for (let el in tags) {
+        tagsArr.push([el, tags[el]]);
       }
-    })
-  }
-  // 태그/재료/북마크/게시 로 분류된 게시물 요청 시
-  else {
-    // tag/ingredient 로 요청 시
-    if (req.query.type === 'tag' || req.query.type === 'ingredient') {
-      const queryInfo = {
+      for (let i = 0; i < tagsArr.length; i++) {
+        tagsArr.sort((a, b) => b[1] - a[1]);
+      }
+      // 상위 재료 배열로 만들어 sort
+      const ingredientsArr = [];
+      for (let el in ingredients) {
+        ingredientsArr.push([el, ingredients[el]]);
+      }
+      for (let i = 0; i < ingredientsArr.length; i++) {
+        ingredientsArr.sort((a, b) => b[1] - a[1]);
+      }
+      // 게시물 최신순으로 전달
+      const articleList = await article.findAll({
         attributes: ['id', 'title', 'thumbnail_type', 'thumbnail_color', 'ingredient'],
         limit: 10,
         offset: 0,
         order: [
           ['createdAt', 'DESC'],
         ],
-        where: {}
-      };
-      // where 조건 동적 할당
-      queryInfo.where[req.query.type] = {
-        [Op.substring]: `${req.query.value}`
-      };
-      // queryInfo 를 바탕으로 검색 및 전달
-      const sortedByType = await article.findAll(queryInfo);
-      sortedByTag.forEach(data => {
+      })
+      articleList.forEach(data => {
         data.thumbnail_color = JSON.parse(data.thumbnail_color);
         data.ingredient = JSON.parse(data.ingredient);
-      });
-      res.json({ data: sortedByType });
-    }
-    // liked/published 로 요청 시
-    else if (req.query.type === 'liked' || req.query.type === 'published') {
-      const queryInfo = {
-        attributes: ['id', 'title', 'thumbnail_type', 'thumbnail_color', 'ingredient'],
-        limit: 10,
-        offset: 0,
-        order: [
-          ['createdAt', 'DESC'],
-        ],
-        where: {}
-      };
-      // liked 인 경우
-      if (req.query.type === 'liked') {
-        const liked = await user.findOne({
-          attributes: ['liked'],
-          where: {
-            id: req.query.value
-          }
-        });
-        // 배열 liked 정보 바탕으로 where 조건 구성
-        queryInfo.where['id'] = {
-          [Op.or]: JSON.parse(liked.liked)
+      })
+      res.status(200).json({
+        data: {
+          tags: tagsArr.slice(0, 4),  // 상위 태그 5개
+          ingredients: ingredientsArr.slice(0, 4),  // 상위 재료 5개
+          articleList
         }
-        const sortedByLiked = await article.findAll(queryInfo);
-        sortedByLiked.forEach(data => {
+      })
+    }
+    // 태그/재료/북마크/게시 로 분류된 게시물 요청 시
+    else {
+      // tag/ingredient 로 요청 시
+      if (req.query.type === 'tag' || req.query.type === 'ingredient') {
+        const queryInfo = {
+          attributes: ['id', 'title', 'thumbnail_type', 'thumbnail_color', 'ingredient'],
+          limit: 10,
+          offset: 0,
+          order: [
+            ['createdAt', 'DESC'],
+          ],
+          where: {}
+        };
+        // where 조건 동적 할당
+        queryInfo.where[req.query.type] = {
+          [Op.substring]: `${req.query.value}`
+        };
+        // queryInfo 를 바탕으로 검색 및 전달
+        const sortedByType = await article.findAll(queryInfo);
+        sortedByTag.forEach(data => {
           data.thumbnail_color = JSON.parse(data.thumbnail_color);
           data.ingredient = JSON.parse(data.ingredient);
         });
-        res.json({ data: sortedByLiked })
+        res.json({ data: sortedByType });
       }
-      // published 인 경우
-      else if (req.query.type === 'published') {
-        // author_id 기준으로 where 조건 구성
-        queryInfo.where['author_id'] = req.query.value;
+      // liked/published 로 요청 시
+      else if (req.query.type === 'liked' || req.query.type === 'published') {
+        const queryInfo = {
+          attributes: ['id', 'title', 'thumbnail_type', 'thumbnail_color', 'ingredient'],
+          limit: 10,
+          offset: 0,
+          order: [
+            ['createdAt', 'DESC'],
+          ],
+          where: {}
+        };
+        // liked 인 경우
+        if (req.query.type === 'liked') {
+          const liked = await user.findOne({
+            attributes: ['liked'],
+            where: {
+              id: req.query.value
+            }
+          });
+          // 배열 liked 정보 바탕으로 where 조건 구성
+          queryInfo.where['id'] = {
+            [Op.or]: JSON.parse(liked.liked)
+          }
+          const sortedByLiked = await article.findAll(queryInfo);
+          sortedByLiked.forEach(data => {
+            data.thumbnail_color = JSON.parse(data.thumbnail_color);
+            data.ingredient = JSON.parse(data.ingredient);
+          });
+          res.json({ data: sortedByLiked })
+        }
+        // published 인 경우
+        else if (req.query.type === 'published') {
+          // author_id 기준으로 where 조건 구성
+          queryInfo.where['author_id'] = req.query.value;
 
-        const sortedByPublished = await article.findAll(queryInfo);
-        sortedByPublished.forEach(data => {
-          data.thumbnail_color = JSON.parse(data.thumbnail_color);
-          data.ingredient = JSON.parse(data.ingredient);
-        });
-        res.json({ data: sortedByPublished })
+          const sortedByPublished = await article.findAll(queryInfo);
+          sortedByPublished.forEach(data => {
+            data.thumbnail_color = JSON.parse(data.thumbnail_color);
+            data.ingredient = JSON.parse(data.ingredient);
+          });
+          res.json({ data: sortedByPublished })
+        }
       }
     }
+  } catch (error) {
+    console.log(error);
+    res.send("sorry");
   }
 };
--- server/controllers/articles/likebtn.js
+++ server/controllers/articles/likebtn.js
@@ -1,9 +1,43 @@
-const { article } = require("../../db/models");
+const { user, article } = require("../../db/models");
 
-module.export = (req, res) => {
-  getarticle: "hi";
-
-
-  // insert into user (username, email, password) values ("kimcoding", "kimcoding@gmail.com", "password");
-  // insert into article (author_id, title, thumbnail_type, thumbnail_color, content, tag, ingredient) values (5, "나폴리탄", "gradient", "red", "잘 섞어서 흔들어 주세요", "태그1, 짠맛", "소금, 설탕");
+module.exports = (req, res) => {
+  try {
+    const { user_id, article_id, likebtn } = req.body;
+    // user.liked 배열 가져오기
+    const userLiked = await findOne({
+      where: {
+        id: user_id
+      }
+    });
+    userLikedArr = JSON.parse(userLiked.liked);
+    userLikedArr.push(article_id);
+    // user.liked 수정 반영하기
+    user.update({
+      liked: JSON.stringify(userLiked)
+    }, {
+      where: {
+        id: user_id
+      }
+    })
+    // article.liked_user_id 배열 가져오기
+    const articleLiked = await findOnd({
+      where: {
+        id: article_id
+      }
+    });
+    articleLikedArr = JSON.parse(articleLiked.like_user_id);
+    articleLikedArr.push(user_id);
+    // article.liked_user_id 수정 반영하기
+    article.update({
+      liked_user_id: JSON.stringify(articleLikedArr)
+    }, {
+      where: {
+        id: article_id
+      }
+    });
+    res.send("Article deleted successfully");
+  } catch (error) {
+    console.log(error);
+    res.send("sorry");
+  }
 };
