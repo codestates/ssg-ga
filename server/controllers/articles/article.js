034f315612b6ba51bd5bc401942acbd975fae6f6
kimeensung <kimeensung@gmail.com>
2021-08-10 15:14:30 +0900

    Server/modify: 게시물 분류해서 전달하는 기능 추가 및 세부 구현 수정

--- server/controllers/articles/article.js
+++ server/controllers/articles/article.js
@@ -1,19 +1,23 @@
-const { article } = require("../../db/models");
+const { article, user } = require("../../db/models");
 
 module.exports = {
   get: async (req, res) => {
     const articleId = req.params.articleId;
-    // articleId 로 해당 게시물 검색
+    // articleId 로 해당 게시물 검색    
     const singleArticle = await article.findOne({
       where: {
         id: articleId
+      },
+      include: {
+        model: user,
+        attributes: ['username', 'image'],
+        as: 'author'
       }
     })
     // JSON 정보 해독
     singleArticle.thumbnail_color = JSON.parse(singleArticle.thumbnail_color);
     singleArticle.tag = JSON.parse(singleArticle.tag);
     singleArticle.ingredient = JSON.parse(singleArticle.ingredient);
-    // data 에 담아 전송
     res.status(200).json({ data: singleArticle });
   },
   post: async (req, res) => {
@@ -34,16 +38,15 @@ module.exports = {
   patch: async (req, res) => {
     const articleId = req.params.articleId
     const { title, thumbnail_type, thumbnail_color, content, tag, ingredient } = req.body;
-
-    const data = {};
-    if (title) data.title;
-    if (thumbnail_type) data.thumbnail_type = thumbnail_type;
-    if (thumbnail_color) data.thumbnail_color = thumbnail_color;
-    if (content) data.content = content;
-    if (tag) data.tag = tag;
-    if (ingredient) data.ingredient = ingredient;
-
-    await article.update(data, {
+    // 게시물 수정
+    await article.update({
+      title,
+      thumbnail_type,
+      thumbnail_color: JSON.stringify(thumbnail_color),
+      content,
+      tag: JSON.stringify(tag),
+      ingredient: JSON.stringify(ingredient)
+    }, {
       where: {
         id: articleId
       }
