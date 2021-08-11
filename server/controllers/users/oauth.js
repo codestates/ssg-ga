ba8d7d65f9b4301855784256aed7d908730a63f3
Hsource39 <source390@gmail.com>
2021-08-11 23:23:08 +0900

    add:kakao oauth 기능 구현

--- server/controllers/users/oauth.js
+++ server/controllers/users/oauth.js
@@ -1,5 +1,11 @@
 const { user } = require("../../db/models");
 const axios = require("axios");
+const bcrypt = require("bcrypt");
+const {
+  generateAccessToken,
+  generateRefreshToken,
+  sendToken,
+} = require("../tokenFunctions");
 require("dotenv").config();
 
 module.exports = (req, res) => {
@@ -9,21 +15,59 @@ module.exports = (req, res) => {
     headers: {
       "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
     },
-  });
-  // .then((response) => {
-  //   console.log(response.data);
-  //   res.status(200).send("ok");
+  }).then((response) => {
+    axios({
+      method: "get",
+      url: `https://kapi.kakao.com/v2/user/me?access_token=${response.data.access_token}`,
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+      },
+    }).then((data) => {
+      user
+        .findOne({
+          where: {
+            email: data.data.kakao_account.email,
+          },
+        })
+        .then(async (dbdata) => {
+          if (dbdata) {
+            delete dbdata.dataValues.password;
+            delete dbdata.dataValues.iat;
+            delete dbdata.dataValues.exp;
+            console.log(JSON.stringify(dbdata.dataValues));
+            const tokenA = generateAccessToken(dbdata.dataValues);
+            const tokenR = generateRefreshToken(dbdata.dataValues);
 
-  //   axios({
-  //     method: "post",
-  //     url: `kapi.kakao.com/v2/user/me`,
-  //     headers: {
-  //       authorization: `Bearer ${response.data.access_token}`,
-  //       "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-  //     },
-  //   }).then((data) => {
-  //     console.log(data);
-  //     res.send("왔다");
-  //   });
-  // });
+            sendToken(res, tokenA, tokenR);
+          } else {
+            let password = data.data.id + data.data.properties.nickname;
+            const salt = await bcrypt.genSalt(5);
+            password = await bcrypt.hash(password, salt);
+
+            user
+              .create({
+                email: data.data.kakao_account.email,
+                username: data.data.properties.nickname,
+                password: password,
+                image: data.data.properties.profile_image,
+              })
+              .then(() => {
+                user
+                  .findOne({
+                    where: {
+                      email: data.data.kakao_account.email,
+                    },
+                  })
+                  .then((newdata) => {
+                    console.log(JSON.stringify(newdata.dataValues));
+                    const tokenA = generateAccessToken(newdata.dataValues);
+                    const tokenR = generateRefreshToken(newdata.dataValues);
+
+                    sendToken(res, tokenA, tokenR);
+                  });
+              });
+          }
+        });
+    });
+  });
 };
