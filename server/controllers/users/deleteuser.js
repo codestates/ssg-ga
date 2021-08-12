133dea14f313b771a0369e495c1b69bf537199a4
Hsource39 <source390@gmail.com>
2021-08-12 15:31:24 +0900

    fix: 코드 수정

--- server/controllers/users/deleteuser.js
+++ server/controllers/users/deleteuser.js
@@ -3,35 +3,44 @@ const { isAuthorized_access } = require("../tokenFunctions");
 const bcrypt = require("bcrypt");
 
 module.exports = async (req, res) => {
-  const userdata = isAuthorized_access(req);
-  await user
-    .findOne({
-      where: { id: userdata.id },
-    })
-    .then(async (data) => {
-      if (!data) {
-        res.status(500).send("500 err sorry");
-      }
-      const validPassword = await bcrypt.compare(
-        req.body.password,
-        data.dataValues.password
-      );
-      if (!validPassword) {
-        res.status(401).send("Current Password Wrong!");
-      } else {
-        user
-          .destroy({
-            where: {
-              id: userdata.id,
-            },
-          })
-          .then((result) => {
-            res
-              .clearCookie("jwtA")
-              .clearCookie("jwtR")
-              .status(200)
-              .send("Delete success...");
-          });
-      }
-    });
+  try {
+    const userdata = isAuthorized_access(req);
+    //삭제 요청 id 확인
+    await user
+      .findOne({
+        where: { id: userdata.id },
+      })
+      .then(async (data) => {
+        if (!data) {
+          res.status(500).send("500 err sorry");
+        }
+        const validPassword = await bcrypt.compare(
+          req.body.password,
+          data.dataValues.password
+        );
+        //삭제할 때 입력한 비밀번호가 틀릴 경우
+        if (!validPassword) {
+          res.status(401).send("Current Password Wrong!");
+        }
+        // 비밀번호 맞을 경우
+        else {
+          user
+            .destroy({
+              where: {
+                id: userdata.id,
+              },
+            })
+            .then((result) => {
+              res
+                .clearCookie("jwtA")
+                .clearCookie("jwtR")
+                .status(200)
+                .send("Delete success...");
+            });
+        }
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("500 err sorry");
+  }
 };
--- server/controllers/users/signin.js
+++ server/controllers/users/signin.js
@@ -8,39 +8,41 @@ const {
 const cryptoJS = require("crypto-js");
 
 module.exports = (req, res) => {
-  user
-    .findOne({
-      where: {
-        email: req.body.email,
-      },
-    })
-    .then(async (data) => {
-      if (!data) {
-        return res.status(404).send("Login fail!");
-      }
-      let decodePassword = cryptoJS.AES.decrypt(
-        req.body.password,
-        process.env.CRYPTOJS_SECRETKEY
-      ).toString();
-      const validPassword = await bcrypt.compare(
-        decodePassword,
-        data.dataValues.password
-      );
-      console.log(validPassword);
-      if (validPassword) {
-        delete data.dataValues.password;
-        delete data.dataValues.iat;
-        delete data.dataValues.exp;
-        console.log(JSON.stringify(data.dataValues));
-        const tokenA = generateAccessToken(data.dataValues);
-        const tokenR = generateRefreshToken(data.dataValues);
+  try {
+    user
+      .findOne({
+        where: {
+          email: req.body.email,
+        },
+      })
+      .then(async (data) => {
+        if (!data) {
+          return res.status(404).send("Login fail!");
+        }
+        let decodePassword = cryptoJS.AES.decrypt(
+          req.body.password,
+          process.env.CRYPTOJS_SECRETKEY
+        ).toString();
+        const validPassword = await bcrypt.compare(
+          decodePassword,
+          data.dataValues.password
+        );
+        console.log(validPassword);
+        if (validPassword) {
+          delete data.dataValues.password;
+          delete data.dataValues.iat;
+          delete data.dataValues.exp;
+          console.log(JSON.stringify(data.dataValues));
+          const tokenA = generateAccessToken(data.dataValues);
+          const tokenR = generateRefreshToken(data.dataValues);
 
-        sendToken(res, tokenA, tokenR);
-      } else {
-        return res.status(409).send("Login fail!");
-      }
-    })
-    .catch((err) => {
-      return res.status(500).send("500 err sorry");
-    });
+          sendToken(res, tokenA, tokenR);
+        } else {
+          return res.status(409).send("Login fail!");
+        }
+      });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("500 err sorry");
+  }
 };
