f2d3a81a01d26ff6adcf8c1b76128bd75d000019
Tae-Gyeong Noh <ntk10912@gmail.com>
2021-08-13 16:51:52 +0900

    add: 로그인 상태에 따른 기능 제한 추가

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,10 +1,12 @@
-import { Link, useLocation } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import RecipeList from "../components/RecipeList";
 import TagRanking from "../components/TagRanking";
 import styled from "styled-components";
 import theme from "../style/theme";
 import queryString from "query-string";
 import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setModal, showModal } from "../actions";
 
 // 메인 페이지 스타일 컴포넌트
 const MainContainer = styled.div`
@@ -26,8 +28,9 @@ const MainContainer = styled.div`
   > #writeBtnWrap {
     display: flex;
     justify-content: flex-end;
-    margin: 20px 0;
-    > a {
+    /* margin: 20px 50px; */
+    margin-right: 40px;
+    > div {
       text-align: center;
       line-height: 50px;
       width: 50px;
@@ -35,14 +38,22 @@ const MainContainer = styled.div`
       border-radius: 50%;
       background-color: blue;
       color: white;
+      cursor: pointer;
     }
   }
 `;
 
 export default function Main() {
   const { search } = useLocation();
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.userReducer);
   const [title, setTitle] = useState("전체보기");
   const query = queryString.parse(search);
+  const popModal = () => {
+    dispatch(setModal(true));
+    dispatch(showModal(true));
+  };
 
   useEffect(() => {
     if (query.mostLiked) {
@@ -61,7 +72,13 @@ export default function Main() {
       <TagRanking query={query} />
       <h1 id="tagTitleWrap">{title}</h1>
       <div id="writeBtnWrap">
-        <Link to="/write">등록</Link>
+        <div
+          onClick={() => {
+            state.isLogin ? history.push("/write") : popModal();
+          }}
+        >
+          등록
+        </div>
       </div>
       {/* 비로그인시 로그인창 띄우기 */}
       <RecipeList query={query} />
