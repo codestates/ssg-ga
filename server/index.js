05606914aa37855c9eee979c01720700758bae30
Hsource39 <81743424+Hsource39@users.noreply.github.com>
2021-08-25 15:14:03 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://ssg-ga.click"],
+    origin: ["https://ssg-ga.click"],
     credentials: true,
     methods: ["GET", "POST", "OPTIONS", "PATCH", "DELETE"],
   })
