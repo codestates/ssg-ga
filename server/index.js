d4a37d30a2d2ad4ac4028125f45b142c55ffde02
Hsource39 <source390@gmail.com>
2021-08-27 18:36:44 +0900

    origin local삭제

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["https://ssg-ga.click", "http://localhost:3000"],
+    origin: ["https://ssg-ga.click"],
     credentials: true,
     methods: ["GET", "POST", "OPTIONS", "PATCH", "DELETE"],
   })
