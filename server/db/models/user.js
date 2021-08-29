e9b7db53a2f250f92c8d4a9956bf0a76d3434306
Hsource39 <source390@gmail.com>
2021-08-29 17:19:45 +0900

    model user social이 빠졌음

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -21,6 +21,7 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         defaultValue: "[]",
       },
+      social: DataTypes.STRING,
     },
     {
       sequelize,
